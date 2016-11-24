7dd9257588e3f8b1fe3efef9976acb1152f94785
AmitBarami <amitbarami@hotmail.com>
2016-11-25 01:59:39 +0200

    Add windows client v1 for legacy reasons

---
+++ client-windows/v1/AnimalName/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AnimalName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(20, 18);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 194);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(263, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(863, 194);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Location = new System.Drawing.Point(0, 254);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1149, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "שמות: 0 || פוטנציאל קומבינציות: 0 || שמות שנותרו לומר: 0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 273);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Miriam", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AnimalName v2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ client-windows/v1/AnimalName/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace AnimalName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<string> animals = null, startListNames = null, listNames = null;
+        private string path = string.Empty;
+        private int combi = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(path = Directory.GetCurrentDirectory(), "*.txt");
+            for (int i = 0; i < files.Length; i++)
+                files[i] = files[i].Replace(path + "\\", "");
+            animals = files.ToList();
+            listBox1.Items.AddRange(files);
+        }
+        private void ReloadList(string file)
+        {
+            startListNames = File.ReadAllLines(file, Encoding.Default).ToList();
+            listNames = new List<string>(startListNames);
+            combi = 0;
+            foreach (var i in startListNames)
+            {
+                int tmp = i.Count(c => c == '(' || c == '[');
+                combi += 1 + (tmp > 0 ? (int)Math.Pow(tmp, tmp) : 0);
+            }
+        }
+        private void RandomName()
+        {
+            if (listNames.Count > 0)
+            {
+                int idx = new Random().Next(listNames.Count);
+                label1.Text = listNames[idx];
+                listNames.RemoveAt(idx);
+                label2.Text = "שמות: " + startListNames.Count + " || פוטנציאל קומבינציות: " + combi + " || שמות שנותרו לומר: " + listNames.Count;
+            }
+            else
+                listNames = new List<string>(startListNames);
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+            ReloadList(path + "\\" + animals[listBox1.SelectedIndex]);
+            RandomName();
+        }
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+            ReloadList(path + "\\" + animals[listBox1.SelectedIndex]);
+            RandomName();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            RandomName();
+        }
+    }
+}
