129fa3bc93285471a7375131820e73a1a4f1d863
Avidor <avidor98@gmail.com>
2016-12-18 22:11:35 +0200

    Small refactor with renames Add output messages

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,22 +2,31 @@ import sqlite3
 
 
 def main():
-    # todo read remote .txt file from dropbox using their API @ https://www.dropbox.com/developers-v1/core/start/python
     print '> Initialising...'
-    convert('sourceFile.txt', '../server/wildDbName.db')
+    dbximport('sourceFile.txt')
+    populate('sourceFile.txt', 'wildDbName.db')  # todo add argparse instead of hardcoded filenames
+    print '\n> Done'
 
 
-def convert(srcfile, destdb):
+def dbximport(dbxfile):  # todo request dbx permission on first usage
+    print '\n> Importing source file from Dropbox ({0})'.format(dbxfile)
+    # https://www.dropbox.com/developers/documentation/python#tutorial
+
+
+def populate(srcfile, destdb):
+    print '\n> Populating database ({0} -> {1})'.format(srcfile, destdb)
     try:
         conn = sqlite3.connect(destdb)
         conn.text_factory = str
         c = conn.cursor()
+        print '>> Cleaning'
         c.execute('DELETE FROM namedb')
         c.execute("DELETE FROM sqlite_sequence WHERE name = 'namedb'")
         with open(srcfile) as f:
             for line in f:
                 print '>> Appending {0}'.format(line.rstrip())
                 c.execute('INSERT INTO namedb (name) VALUES (?)', (line.rstrip(),))
+        print '>> Saving...'
         conn.commit()
         conn.close()
     except sqlite3.Error as e:
